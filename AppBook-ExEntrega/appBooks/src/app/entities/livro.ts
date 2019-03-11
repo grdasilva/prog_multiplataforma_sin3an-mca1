d569b2ebed9db0f3dff07cb549e150545267f031
grdasilva <guirod961@gmail.com>
2019-03-10 23:50:10 -0300

    Entrega Exercicio AppBooks

---
+++ AppBook-ExEntrega/appBooks/src/app/entities/livro.ts
@@ -0,0 +1,10 @@
+export class Livro {
+    titulo:string;
+    subtitulo:string;
+    capa:string;
+    editora:string;
+    autor:string;
+    isbn:string;
+    publicacao:string;
+    paginas:string;
+}
